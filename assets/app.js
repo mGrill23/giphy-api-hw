cde98c33d60ae251859c60ba303e3893f4c19a19
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2018-11-03 09:17:55 -0400

    WorkingOnFunctionallity

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,26 @@
-var queryUrl = "http://api.giphy.com/v1/gifs/random?q=" + searchQuery + "&api_key=WAXRpkX9VGr1jNv1fqExoE3TPhzI3JNM";
+var queryUrl = "http://api.giphy.com/v1/gifs/random?q=" + searchQuery + "&api_key=WAXRpkX9VGr1jNv1fqExoE3TPhzI3JNM&limit=10";
 var searchQuery = "placeholder";
-var animals = [];
+var animals = ["birds", "cats", "dogs", "crabs", "emu", "snakes"];
 
-var animalBtn = $("<button").addClass(".animal-button");
+
+function createNewBtns() {
+
+    $("#buttons").clear();
+
+    for(var i = 0; i < animals.length; i++){
+    
+    var animalBtn = $("<button").addClass(".animal-button");
+
+    animalBtn.attr("data-animal", animals[i]);
+
+    animalBtn.text(animals[i]);
+
+    $("#buttons").append(animalBtn);
+
+    }
+
+}
+
+
 
 
