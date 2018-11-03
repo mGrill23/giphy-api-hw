9d3b1eee28d4245925f44f0e09e156d238188104
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2018-11-02 20:08:29 -0400

    JsWork

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,7 @@
-var queryUrl = $.get("http://api.giphy.com/v1/gifs/random?q=" + searchQuery + "&api_key=WAXRpkX9VGr1jNv1fqExoE3TPhzI3JNM");
+var queryUrl = "http://api.giphy.com/v1/gifs/random?q=" + searchQuery + "&api_key=WAXRpkX9VGr1jNv1fqExoE3TPhzI3JNM";
 var searchQuery = "placeholder";
+var animals = [];
+
+var animalBtn = $("<button").addClass(".animal-button");
+
+
